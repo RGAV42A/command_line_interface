32e035bd47cdc51779dd2dcb9353a9802f816ec9
RGAV42A <ragav@4261811f904c>
2021-03-03 05:26:24 +0000

    update

--- cli-tutorial/cli_tutorial/cli.py
+++ cli-tutorial/cli_tutorial/cli.py
@@ -1,25 +1,42 @@
 import click
 import pandas as pd
 from icecream import ic
+from funcy import identity
+import os
 
 
 @click.command()
-@click.option("--in", "-i", "in_file", required=True,help="Path to csv file to be processed.")
-@click.option("--out-file", "-o", default="./output.xlsx",help="Path to excel file to store the result.")
+@click.option("--in", "-i", "in_file", required=True,help="Path to csv file to be processed.",
+    type=click.Path(exists=True, dir_okay=False, readable=True),)
+@click.option("--out", "-o","out_file", default="output.xlsx",help="Path to excel file to store the result.",
+    type=click.Path(dir_okay=False),)
+@click.option('--verbose', is_flag=True, help="Verbose output")
+@click.option("--dev", "server_url", help="Upload to dev server",
+    flag_value='/home/ragav/GITdir/command_line_interface/cli-tutorial/cli_tutorial/dev',)
+@click.option("--test", "server_url", help="Upload to test server",
+    flag_value='/home/ragav/GITdir/command_line_interface/cli-tutorial/cli_tutorial/test',)
+@click.option("--prod", "server_url", help="Upload to prod server",
+    flag_value='/home/ragav/GITdir/command_line_interface/cli-tutorial/cli_tutorial/prod',default=True)
 
-
-
-def process(in_file, out_file):
+def process(in_file, out_file,verbose,server_url):
     """
     Processes the input file IN and stores the result to
     output file OUT.
     """
-    input = csv_read(in_file)
-    ic(input)
-    output = process_csv(input)
-    ic(output)
-    write_excel(output, out_file)
 
+    print_func = print if verbose else identity
+    print_func("We will start with the input")
+    input = csv_read(in_file)
+    if verbose is False:
+        ic(input)
+    print_func("Next we procees the data")
+    output = process_csv(input)
+    if verbose is False:
+        ic(output)
+    print_func("Finally, we dump it")
+    write_excel(output, out_file)
+    print_func("Upload it to the server")
+    upload_to(server_url, output)
 
 def csv_read(path):
     try:
@@ -42,6 +59,10 @@ def process_csv(input):
 def write_excel(output,out_file):
     with pd.ExcelWriter(out_file) as writer:
         output.to_excel(writer)
+
+def upload_to(server_url, output):
+    os.path.join('a', 'b', 'c')
+
 
 if __name__ =="__main__":
     process()
